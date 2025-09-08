08c592e45cfb39782c0db682440da0ba7de53327
raxmux <raxmuxdk@gmail.com>
2025-09-08 17:02:04 +0200

    player list

--- TBS/TBS/PlayerCreator.cs
+++ TBS/TBS/PlayerCreator.cs
@@ -1,11 +1,14 @@
 ﻿public class PlayerCreator
 {
+    public List<Player> players = new List<Player>();
     public Player PlayerCreatorFunction()
     {
         Console.WriteLine("Name your character (needed to login)");
         string name = Console.ReadLine();
         Console.WriteLine("Write a password for you character (needed to login)");
         string password = Console.ReadLine();
-        return new Player(name, password, "Pathfinder", 1, 0, 100, 10, 10, 0, 105, 100, 105, 200, 105, 100, 100);
+        Player newPlayer = new Player(name, password, "Pathfinder", 1, 0, 100, 10, 10, 0, 105, 100, 105, 200, 105, 100, 100);
+        players.Add(newPlayer);
+        return newPlayer;
     }
 }
