6bca1fe0df7d82bef64202e76d60fbbc6f1ae4ac
raxmux <raxmuxdk@gmail.com>
2025-09-08 16:20:48 +0200

    Create Player.cs

---
+++ TBS/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+
+class Player
+{
+    //stats
+    string playerName;
+    string password;
+    string playerClass;
+    int level;
+    int exp;
+    int HP;
+    int DMG;
+    int speed;
+    int armor;
+
+    int dodge;
+    int dodgeNegation;
+    int critChance;
+    int critDamage;
+    int stun;
+    int stunNegation;
+
+    int luck;
+
+    public Player(string TplayerName, string Tpassword, string TplayerClass, int Tlevel, int Texp, int THP, int TDMG, int Tspeed, int Tarmor, 
+        int Tdodge, int TdodgeNegation, int Tcritchance, int TcritDamage, int Tstun, int TstunNegation, int Tluck)
+    {
+        playerName = TplayerName;
+        password = Tpassword;
+        playerClass = TplayerClass;
+        level = Tlevel;
+        exp = Texp;
+        HP = THP;
+        DMG = TDMG;
+        speed = Tspeed;
+        armor = Tarmor;
+        dodge = Tdodge;
+        dodgeNegation = TdodgeNegation;
+        critChance = Tcritchance;
+        critDamage = TcritDamage;
+        stun = Tstun;
+        stunNegation = TstunNegation;
+        luck = Tluck;
+    }
+}
