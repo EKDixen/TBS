203bee9b63632126be21720b2d81bb0398b08181
raxmux <raxmuxdk@gmail.com>
2025-09-09 10:01:40 +0200

    øhhh, type lort? Til david (Location ting)

--- TBS/TBS/JourneyManager.cs
+++ TBS/TBS/JourneyManager.cs
@@ -2,13 +2,14 @@
 
 class JourneyManager
 {
+    public Location currentLocation;
     public List<Location> locations = new List<Location>();
     public List<Location> knownLocations = new List<Location>();
 
-    Location starterTown = new Location(true,"StarterTown");
-    Location forest = new Location(false, "Forest");  
-    Location mountain = new Location(false, "Mountain");
-    Location lake = new Location(false, "Lake");
+    Location starterTown = new Location(true,"StarterTown",new System.Numerics.Vector2(0,0));
+    Location forest = new Location(false, "Forest",new System.Numerics.Vector2(1, 0));  
+    Location mountain = new Location(false, "Mountain",new System.Numerics.Vector2(-1, 0));
+    Location lake = new Location(false, "Lake",new System.Numerics.Vector2(0, -1));
 
     public void AddLocations()
     {
@@ -53,10 +54,24 @@ class JourneyManager
     public void Travel(Location TtargetDis)
     {
         Console.WriteLine("\ngoing to " + TtargetDis.name);
+        currentLocation = TtargetDis;
     }
     public void Explore()
     {
         Console.WriteLine("\nexploring");
+        Random random = new Random();
+        int locationFound = random.Next(1,4);
+        if (locationFound == 1)
+        {
+            for (int i = 0; i < knownLocations.Count - 1; i++)
+            {
+                if (locations[i].location == currentLocation.location + new System.Numerics.Vector2(1, 0) && 
+                (knownLocations[i].location == currentLocation.location + new System.Numerics.Vector2(1, 0)))
+                {
+
+                }
+            }
+        }
     }
 
 
--- TBS/TBS/Location.cs
+++ TBS/TBS/Location.cs
@@ -1,13 +1,17 @@
-﻿class Location
+﻿using System.Numerics;
+
+class Location
 {
     public string name;
     public bool known;
+    public Vector2 location;
 
 
-    public Location(bool Tknown, string Tname)
+    public Location(bool Tknown, string Tname, Vector2 Tlocation)
     {
         known = Tknown;
         name = Tname;
+        location = Tlocation;
     }
 
 }
