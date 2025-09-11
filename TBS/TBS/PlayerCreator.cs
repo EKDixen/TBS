dc17a31e36709269c42819191c298d133ffc069b
raxmux <raxmuxdk@gmail.com>
2025-09-11 19:57:01 +0200

    Location til player

--- TBS/TBS/PlayerCreator.cs
+++ TBS/TBS/PlayerCreator.cs
@@ -7,7 +7,7 @@
         string name = Console.ReadLine();
         Console.WriteLine("Write a password for you character (needed to login)");
         string password = Console.ReadLine();
-        Player newPlayer = new Player(name, password, "Pathfinder", 1, 0, 100, 10, 10, 0, 105, 100, 105, 200, 105, 100, 10, 100);
+        Player newPlayer = new Player(name, password, "Pathfinder", 1, 0, 100, 10, 10, 0, 105, 100, 105, 200, 105, 100, new, float[] { 0, 0 }, 10, 100);
         players.Add(newPlayer);
         return newPlayer;
     }
