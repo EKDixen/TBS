cb129ec649f5ead1111b09fdf51ac08df23db4d8
Darwin-our-lord <David@prebensen.dk>
2025-09-15 10:45:32 +0200

    okay test over it works

--- TBS/TBS/Inventory.cs
+++ TBS/TBS/Inventory.cs
@@ -1,17 +1,16 @@
 ﻿using Game.Class;
 public class Inventory
 {
-    Item item = new Item("name","description",1,ItemType.equipment);
+    /*Item item = new Item("name","description",1,ItemType.equipment);
     Item item2 = new Item("name2", "description2", 16,ItemType.equipment);
+                                                                             ------this is just for test
     public void AddItems()
     {
         Program.player.ownedItems.Add(item);
         Program.player.ownedItems.Add(item2);
-    }
+    } */
     public void ShowInventory()
     {
-        AddItems();
-
         Console.WriteLine("\n     Name            Qty   Description");
         Console.WriteLine("--------------------------------------");
         int i = 0;
@@ -21,6 +20,7 @@ public class Inventory
             Console.WriteLine($"{i,-5}{item.name,-15} {item.amount,-5} {item.description}");
         }
         Console.WriteLine("\n \n ");
+        Program.SavePlayer();
         Program.MainMenu();
     }
 
