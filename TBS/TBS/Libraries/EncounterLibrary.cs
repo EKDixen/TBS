0ed9431aeb6728500a91194d5e9eae0549eb71bd
Icey_Carp <Dixenbv@gmail.com>
2025-10-02 09:52:48 +0200

    EncounterLibrary is now static

--- TBS/TBS/Libraries/EncounterLibrary.cs
+++ TBS/TBS/Libraries/EncounterLibrary.cs
@@ -1,4 +1,6 @@
-﻿public class EncounterLibrary
+﻿public static class EncounterLibrary
 {
+    public static Encounter FindCoins = new Encounter("FoundCoins", 100, null, false);
 
+    public static Encounter LostCoins = new Encounter("LostCoins", 100, null, false);
 }
