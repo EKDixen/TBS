c86d842bb6c893f851f8e012dd9a965ac4d099e5
Darwin-our-lord <David@prebensen.dk>
2025-10-07 11:50:00 +0200

    added travel prize:)

--- TBS/TBS/Libraries/LocationLibrary.cs
+++ TBS/TBS/Libraries/LocationLibrary.cs
@@ -3,7 +3,7 @@
 public static class LocationLibrary
 {
 
-    public static Location starterTown = new Location(true, "StarterTown", new System.Numerics.Vector2(0, 0), new List<SubLocation>
+    public static Location starterTown = new Location(true, "StarterTown", new System.Numerics.Vector2(0, 0),0, new List<SubLocation>
     {
         new SubLocation("casino", SubLocationType.casino)
         {
@@ -21,7 +21,7 @@ public static class LocationLibrary
     });
 
 
-    public static Location forest = new Location(false, "Forest", new System.Numerics.Vector2(1, 0), new List<SubLocation>
+    public static Location forest = new Location(false, "Forest", new System.Numerics.Vector2(1, 0),0, new List<SubLocation>
     {
         new SubLocation("name", SubLocationType.shop)
         {
@@ -46,12 +46,12 @@ public static class LocationLibrary
         { EnemyLibrary.VampireSpawn, 100 }
     });
 
-    public static Location mountain = new Location(false, "Mountain", new System.Numerics.Vector2(-1, 0), new List<SubLocation>
+    public static Location mountain = new Location(false, "Mountain", new System.Numerics.Vector2(-1, 0),0, new List<SubLocation>
     {
 
     }, new Dictionary<Enemy, int>());
 
-    public static Location lake = new Location(false, "Lake", new System.Numerics.Vector2(0, -1), new List<SubLocation>
+    public static Location lake = new Location(false, "Lake", new System.Numerics.Vector2(0, -1),0, new List<SubLocation>
     {
 
     }, new Dictionary<Enemy, int>());
--- TBS/TBS/Location.cs
+++ TBS/TBS/Location.cs
@@ -8,18 +8,21 @@ public class Location
     public bool known;
     public Vector2 location;
 
+    public int travelPrize;
+
     public List<SubLocation> subLocationsHere = new List<SubLocation>();
     [JsonIgnore]
     public Dictionary<Enemy, int> PossibleEncounters { get; set; }
 
     public Location() { } // Deserialize
-    public Location(bool Tknown, string Tname, Vector2 Tlocation, List<SubLocation> subLocations, Dictionary<Enemy, int> TpossibleEncounters = null)
+    public Location(bool Tknown, string Tname, Vector2 Tlocation, int travelPrize, List<SubLocation> subLocations, Dictionary<Enemy, int> TpossibleEncounters = null)
     {
         known = Tknown;
         name = Tname;
         location = Tlocation;
         subLocationsHere = subLocations;
         PossibleEncounters = TpossibleEncounters ?? new Dictionary<Enemy, int>();
+        this.travelPrize = travelPrize;
     }
 
 }
