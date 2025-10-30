e60411b44aeb4ccf74ca2297543b282d1bf5a14b
raxmux <raxmuxdk@gmail.com>
2025-10-30 16:25:49 +0100

    bat file update

--- TBS/TBS/RunGame.bat
+++ TBS/TBS/RunGame.bat
@@ -1,2 +1,8 @@
 ﻿@echo off
-start "" /wait conhost.exe cmd /c "cd /d %~dp0 && dotnet run"
+if exist "%~dp0TBS.exe" (
+    start "" /wait conhost.exe cmd /c "cd /d %~dp0 && TBS.exe"
+) else if exist "%~dp0..\bin\Release\net8.0\win-x64\publish\TBS.exe" (
+    start "" /wait conhost.exe cmd /c "cd /d %~dp0..\bin\Release\net8.0\win-x64\publish && TBS.exe"
+) else (
+    start "" /wait conhost.exe cmd /c "cd /d %~dp0 && dotnet run"
+)
