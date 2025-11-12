f05f2b5a059c5c59212258097bdb3b9c6711cfdb
Darwin-our-lord <David@prebensen.dk>
2025-11-12 22:29:49 +0100

    skibidi

--- TBS/TBS/Program.cs
+++ TBS/TBS/Program.cs
@@ -147,9 +147,9 @@ namespace Game.Class
             MainUI.WriteInMainArea("Check Moves : 2");
             MainUI.WriteInMainArea($"Do something at {player.currentLocation} : 3");
             MainUI.WriteInMainArea("Check stats : 4");
-            MainUI.WriteInMainArea("Check Encyclopedia : 5");
+            //MainUI.WriteInMainArea("Check Encyclopedia : 5");
 
-            if (int.TryParse(Console.ReadKey().KeyChar.ToString(), out int input) == false || input > 5 || input < 0)
+            if (int.TryParse(Console.ReadKey().KeyChar.ToString(), out int input) == false || input > 4 || input < 0)
             {
                 MainUI.WriteInMainArea("\nyou gotta type 0, 1, 2, 3, 4, or 5");
                 MainMenu();
@@ -202,7 +202,7 @@ namespace Game.Class
                 }
             }
             else if (input == 4) ShowPlayerStats();
-            else if (input == 5) Inventory.ShowInventory();
+            //else if (input == 5) Inventory.ShowInventory();
 
             db.SavePlayer(player);
         }
